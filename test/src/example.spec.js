ece8133694336ec4d2a717f6dcf53faf34799987
Isaac Helbling <imhelbling@gmail.com>
2017-05-10 21:56:16 -0400

    changed example spec

--- test/src/example.spec.js
+++ test/src/example.spec.js
@@ -3,8 +3,14 @@ const { expect } = require('chai');
 
 describe("Testing", () => {
 
-  it("Should pass", () => {
-    expect(true).to.be.true;
+  beforeEach(()=>{
+    
+  })
+
+  describe("Method", () => {
+    it("Should pass", () => {
+      expect(true).to.be.true;
+    });
   });
-  
+
 });
